d05286ddac6e7dd17acdb91a086efedb438b17b0
Pedro Henrique <kfp3drod15@gmail.com>
2025-03-17 15:57:59 -0300

    Pagina Fale conosco + script and css

--- OasaSite/script.js
+++ OasaSite/script.js
@@ -12,3 +12,54 @@ const linkClicked = (link) => {
 
     content.classList.add('show');
 }
+
+window.onload = function() {
+    emailjs.init("mn9zgmsv_qfhtX5Ik");
+};
+
+
+function sendEmail() {
+
+    let btnEnviar = document.getElementById("btn-enviar");
+    let btnLoader = document.getElementById("btn-enviar-loader");
+
+    btnEnviar.style.display = "none";  // Esconde o botão "Enviar"
+    btnLoader.style.display = "inline-block";  // Mostra o loader
+
+    let nameInput = document.getElementById("name");
+    let emailInput = document.getElementById("email");
+    let messageInput = document.getElementById("message");
+
+    let params = {
+        name: nameInput.value,
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+
+    if (nameInput.value.trim() === "" || emailInput.value.trim() === "" || messageInput.value.trim() === "") {
+        alert("Por favor, preencha todos os campos antes de enviar.");
+        btnEnviar.style.display = "inline-block";  
+        btnLoader.style.display = "none"; 
+        return;
+    }
+
+    console.log("Enviando email com os seguintes parâmetros:", params);
+
+    emailjs.send("service_sal7pe8", "template_5wk2vxq", params, "mn9zgmsv_qfhtX5Ik")
+        .then(() => {
+           console.log("email enviado!");
+           location.reload();
+           nameInput.value = "";
+           emailInput.value = "";
+           messageInput.value = "";
+           
+        })
+        .catch((error) => {
+            console.error("Erro ao enviar email:", error);
+        });
+}
+
+document.getElementById('btn-enviar').addEventListener('click', function (event) {
+    event.preventDefault();
+    sendEmail();
+});

93b95e2a60c5b0bc2130ed28d9749ab55a2bcbbb
setebos07 <44853639+setebos07@users.noreply.github.com>
2025-04-06 22:11:24 -0300

    Add files via upload

--- OasaSite/projetos.js
+++ OasaSite/projetos.js
@@ -1,12 +1,18 @@
 document.querySelectorAll('.project').forEach(project => {
-    project.addEventListener('click', function() {
+  project.addEventListener('click', function() {
       // Primeiro, esconda todos os conteúdos de projetos
-      document.querySelectorAll('.content').forEach(content => {
-        content.style.display = 'none';
+      document.querySelectorAll('.project-content').forEach(content => {
+          content.style.display = 'none';
       });
       
       // Depois, exiba o conteúdo relacionado ao projeto clicado
       const contentId = this.getAttribute('content-id');
-      document.getElementById(contentId).style.display = 'flex';
-    });
+      const targetContent = document.getElementById(contentId);
+      targetContent.style.display = 'flex';
+      
+      // Aplica scroll apenas em dispositivos móveis (largura menor que 768px)
+      if (window.innerWidth <= 768) {
+          targetContent.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
   });
+});
